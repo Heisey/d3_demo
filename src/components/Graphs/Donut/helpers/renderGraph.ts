c05f0d4e253e56758d672e9ea82cfef0211b7d1f
Heisey <jheisler01@gmail.com>
2021-12-01 08:11:56 -0700

    added event

--- src/components/Graphs/Donut/helpers/renderGraph.ts
+++ src/components/Graphs/Donut/helpers/renderGraph.ts
@@ -1,10 +1,25 @@
 import * as d3 from 'd3'
+import React from 'react'
 
 import * as IGraph from '../../interfaces'
 import lib from '../../lib'
 
 import helpers from '../helpers'
 import * as I from '../interfaces'
+
+// const handleMouseOver = (e: React.MouseEvent<SVGElement>, d: d3.PieArcDatum<IGraph.GraphData>) => {
+//   console.log(e)
+//   console.log(d)
+// }
+
+// function handleMouseOver (d: d3.BaseType, i: any, nodes: any) => {
+//   console.log(d)
+//   console.log(i)
+//   console.log(nodes)
+//   // d3.select(this)
+//   //   .style('background-color', `${i.data.fill}20`)
+  
+// }
 
 type RenderDonutGraph<T> = (node: IGraph.D3Node, data: T[], graphStyles: I.DonutStyles) => void
 
@@ -14,6 +29,7 @@ const renderGraph: RenderDonutGraph<IGraph.GraphData> = (node, data, graphStyles
 
   const container = lib.generateSvgContainer(node, size)
   const graphGroup = helpers.generateGraphGroup(container, size, center)
+
 
   const pie = d3.pie<IGraph.GraphData>()
   
@@ -35,6 +51,21 @@ const renderGraph: RenderDonutGraph<IGraph.GraphData> = (node, data, graphStyles
     .attr('class', 'arc')
     .attr('d', arcPath)
     .attr('fill', d => d.data.fill)
+
+    
+  graphGroup
+  .selectAll('path')
+  // .on('mouseover', handleMouseOver)
+  .on('mouseenter', function(actual, i) {
+    // console.log('puppies', this, i)
+    const node = d3.select(this)
+      node.attr('opacity', 0.5)
+      console.log(node)
+      
+  })
+  .on('mouseleave', function(actual, i) {
+    d3.select(this).attr('opacity', 1)
+  })
 }
 
 export default renderGraph
